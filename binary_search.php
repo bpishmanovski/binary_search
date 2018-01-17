9d3f7e1ad3a17b66735172e1138752e526d98cd9
Blagojce Pishmanovski <blagojce.pishmanovski@gmail.com>
2018-01-17 12:07:49 +0100

    first commit

---
+++ binary_search.php
@@ -0,0 +1,39 @@
+// DSSP koja vo niza binarno ke prebaruva nekoj element.
+
+<?php
+$a=[3,7,12,20,28,34,40,57,64,69,75,84,90,97];
+$b = readline("Vnesi broj: ");
+function BinarnoPrebaruvanje(array $a, $b)
+{	
+	$start=0;
+	$end=sizeof($a);
+	$mid=0;
+	$found = false;
+	while($start<=$end)
+	{
+		$mid=floor(($start+$end)/2);
+		
+		if($a[$mid]==$b)
+		{
+			$found=true;
+			$index = $mid;
+			break;
+		}
+		elseif($a[$mid]>$b)
+		{
+			$end=$mid-1;
+		}
+		elseif($a[$mid]<$b)
+		{
+			$start=$mid+1;
+		}
+	}
+	if($found){
+		echo "{$b} is found on position {$index} in the array\n";
+	}
+	else{
+		echo "The number {$b} isn't found in the array\n";
+	}
+}
+
+BinarnoPrebaruvanje($a,$b);
